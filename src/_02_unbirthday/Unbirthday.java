5aae894225cb1b3f2c3310b15a4d0aae0b73c505
League Student <league@iMac-5.attlocal.net>
2019-11-02 13:00:44 -0700

    lvl0 module1 unbday

--- src/_02_unbirthday/Unbirthday.java
+++ src/_02_unbirthday/Unbirthday.java
@@ -1,5 +1,13 @@
 package _02_unbirthday;
 
-public class Unbirthday {
+import javax.swing.JOptionPane;
 
+public class Unbirthday {
+	
+	public static void main(String[] args) {
+String bday = JOptionPane.showInputDialog(null,"Please enter your birthday (mm/dd)");
+if ()
+		
+		
+}
 }
