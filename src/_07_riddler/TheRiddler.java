2da114832418d9496782bf435dc0c4a6b6818175
League Student <league@iMac-9.attlocal.net>
2019-11-16 13:00:20 -0800

    worked on lvl 0 module1

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _07_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,13 +12,20 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String riddle1 = JOptionPane.showInputDialog(null, "Which word in the dictionary is spelled incorrectly? (do not add any capital letters in your answer.)");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if (riddle1.equals ("incorrectly")) {
+	JOptionPane.showMessageDialog(null, "Correct! You got a point!");
+	score++;
+	JOptionPane.showMessageDialog(null,"Your score is:" + score);
+}
+else {
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+JOptionPane.showMessageDialog(null,"WRONG! The correct answer is 'incorrectly'");
+JOptionPane.showMessageDialog(null,"Your score is:" + score);
+}
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
--- src/_09_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_09_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,7 +1,16 @@
 
 void setup() {
-
+PImage face = loadImage("zombieEyes.png");
+image(face,0,0);
+size(600,800);
+face.resize(600, 800);
 }
 void draw() {
-
+  
+  fill(mouseX, 100, mouseY);
+ellipse(372, 211, 100, 100);
+ellipse(248, 202, 125, 125);
+fill(0,0,0);
+  ellipse(372, 211, 25, 25);
+ellipse(248, 202, 30, 30);
 }
